5bad45557fa1a9d9d7fca0f77a8856088b818d9c
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-01-21 18:09:05 +0700

    add new func

--- January 2025/Day4/day4.js
+++ January 2025/Day4/day4.js
@@ -23,7 +23,44 @@ function smallMoveDownB1(){
 
 }
 
-smallMoveDownB1();
+function filterB3(){
+    input_box = document.querySelector('.inputbox');
+    list_item = document.querySelectorAll('.list li');
 
+    input_box.addEventListener('input',()=>{
+        let text_input = input_box.value;
+
+        for(let i = 0; i < list_item.length; i++){
+            if(list_item[i].textContent.substring(0,text_input.length) != text_input){
+                list_item[i].style.display = 'none';
+            }
+            else list_item[i].style.display = 'block';
+        }
+    });
+}
+
+function enlargeImage(){
+    small_pic = document.querySelector('#b5 > .image');
+    big_pic = document.querySelector('.modal');
+    
+    small_pic.addEventListener('click',()=>{
+        big_pic.style.display = 'block';
+    });
+}
+
+function removeModal(){
+    btn = document.querySelector('.modal > .button_out');
+    big_pic = document.querySelector('.modal');
+
+    btn.addEventListener('click',()=>{
+        big_pic.style.display = 'none';
+    });
+}
+
+
+removeModal();
+enlargeImage();
+smallMoveDownB1();
+filterB3();
 
 
