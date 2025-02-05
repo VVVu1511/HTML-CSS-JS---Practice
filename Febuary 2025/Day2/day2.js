70726511ef1306eadd8a7ee0d4ef6fda0d9c7e5d
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-02-05 13:13:51 +0700

    day3

---
+++ Febuary 2025/Day2/day2.js
@@ -0,0 +1,65 @@
+// function Counter(){
+//     this.count = 0;
+//     let interval = setInterval(() => {
+//         this.count++;
+//         console.log(this.count);
+//     },1000);
+
+//     setTimeout(() => {
+//         clearInterval(interval);
+//     },3000);
+// }
+
+// new Counter();
+
+// const number = [1,2,3,4,5];
+
+// const doubled = number.map(n => n ** 2);
+
+// console.log(doubled);
+
+// const number = [1, 2, 3, 4, 5];
+
+// const myList = number.map((item) => <p>{item}</p>);
+
+// function App() {
+//     return (
+//         <div>
+//             {myList}
+//         </div>
+//     );
+// }
+
+// let calculation = (a,b) => {
+//     return [a + b,a - b, a * b, a / b];
+// };
+
+// const [add,subtract,multiply,divide] = calculation(5,6);
+
+// console.log(add,subtract,multiply,divide);
+
+// const test = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 2005,
+//     age: 18,
+// };
+
+// function myVehicle(a,b,c,d){
+//     return 'My ' + a + ' is ' + b + ' and manufactured in ' + c + ' ' + d;
+// }
+
+// const {brand: a,model: b,year: c,age : d} = test;
+
+
+// console.log(myVehicle(a,b,c,d));
+
+
+
+
+
+
+
+
+
+
